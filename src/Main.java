eefb47a3e7186e94508fc03591a97600479794b6
mdaabis <mostafadaabis@gmail.com>
2022-11-07 19:40:58 +0000

    Solution faster than 89% of Java submissions

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,38 @@
 public class Main {
     public static void main(String[] args) {
+        int num = -96669;
+        System.out.println(maximum69Number(num));
+    }
+
+    public static int maximum69Number(int num)
+    {
+        char[] chars = String.valueOf(num).toCharArray();
+        int i = 0;
+        if (num > 0)
+        {
+            for (char character : chars)
+            {
+                if (character == '6')
+                {
+                    chars[i] = '9';
+                    break;
+                }
+                else
+                    i++;
+            }
+        }
+        else {
+            for (char character : chars)
+            {
+                if (character == '9')
+                {
+                    chars[i] = '6';
+                    break;
+                }
+                else
+                    i++;
+            }
+        }
+        return Integer.parseInt(new String(chars));
     }
 }
